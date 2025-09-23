5f1aa9fc7bd5009909d7f777354f4b0c087abde2
ShadowWolve14 <jonas.hever@icloud.com>
2025-09-24 00:35:25 +0200

    eh

---
+++ Assets/Scripting/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>;
+       
+    }
+
+}
