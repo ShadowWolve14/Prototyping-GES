39fbd7f9f9874391ca92ecf002f74eaa7df5cc70
ShadowWolve14 <jonas.hever@icloud.com>
2025-09-24 01:56:04 +0200

    Movement funktionier cam is noch wonky

--- Assets/Scripting/PlayerMovement.cs
+++ Assets/Scripting/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    public float Damping;
 
     public Transform orientation;
 
@@ -18,8 +19,47 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>;
-       
+        rb = GetComponent<Rigidbody>();
+       rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        MyInput();
+        SpeedControl();
+        rb.linearDamping = Damping; 
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        //callculate movement Direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection* moveSpeed, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        //limit velocity if needed
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+
     }
 
 }
