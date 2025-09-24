e33505528c9bd634b107f08fd99afed1892f6211
lilliberker <lilli.berker@web.de>
2025-09-24 15:13:38 +0200

    Revert "Merge branch 'main' into Lilli"
    This reverts commit 5be51c7d72e0a8b8c2e96df531f6180770865dd8, reversing
    changes made to 6244f54efba2a00e9b26e7b72465ea0d7b17ecc1.

--- Assets/Scripting/ThirdPersonCam.cs
+++ Assets/Scripting/ThirdPersonCam.cs
@@ -25,7 +25,7 @@ public class ThirdPersonCam : MonoBehaviour
         //rotate player Object
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 InputDir = -orientation.forward * verticalInput + -orientation.right * horizontalInput;
+        Vector3 InputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (InputDir != Vector3.zero)
         {
